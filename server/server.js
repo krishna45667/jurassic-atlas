7c153403602d6652f8d6ddf0eec5f70db7480396
Krishna_266 <jaiswalkrishna2619@gmail.com>
2026-08-16 14:34:49 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -21,6 +21,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
