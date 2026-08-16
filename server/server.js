62d8f474265e137537c703d0571976dd232479f2
Krishna_266 <jaiswalkrishna2619@gmail.com>
2026-08-17 00:39:36 +0530

    Fix CORS for Vercel preview deployments

--- server/server.js
+++ server/server.js
@@ -27,7 +27,11 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (
+      !origin || 
+      allowedOrigins.includes(origin) || 
+      origin.endsWith('.vercel.app')
+    ) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
